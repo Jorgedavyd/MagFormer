e2eb6454b5ead34c2506de7a7450b489f9e5f185
Jorgedavyd <jorged.encyso@gmail.com>
2024-09-10 10:25:16 -0400

    setting up massive download

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 #Lighting utils for data pipeline automation
+from starstream import Callable, datetime_interval
 from lightning import LightningDataModule
 
 # General utils for Dataset creation
@@ -13,9 +14,11 @@ from starstream.downloaders import SOHO, WIND, ACE, DSCOVR, SWARM, SDO, Dst
 from datetime import timedelta, datetime
 import pandas as pd
 import torch
-from typing import List, Tuple, Sequence
+from typing import Dict, List, Tuple, Sequence
 from torch import Tensor
-
+from PIL import Image
+import os.path as osp
+import glob
 """
 base_scrap_date: Means the scrap date of the Dst index from which
 we are downloading the historic data. We create a relative scrap
@@ -117,19 +120,19 @@ class DatasetLevel3(Dataset):
         self.datasets = args
     def __len__(self) -> int:
         return len(self.datasets[0])
-    def __getitem__(self, idx) -> Sequence[Tensor]:
-        return [dataset[idx] for dataset in self.datasets]
+    def __getitem__(self, idx) -> Tuple[Tensor, ...]:
+        return tuple([dataset[idx] for dataset in self.datasets])
 
 class WINDDataset(L1DatasetLevel1):
     def prepare_data(self, scrap_date, step_size) -> pd.DataFrame:
         dataset = [WIND.TDP_PM(), WIND.MAG()]
-        dataset = list(map(dataset, lambda x: x.data_prep(scrap_date, step_size)))
+        dataset =  list(map(lambda x: x.data_prep(scrap_date, step_size), dataset))
         return pd.concat(dataset, axis = 1)
 
 class ACEDataset(L1DatasetLevel1):
     def prepare_data(self, scrap_date, step_size) -> pd.DataFrame:
         dataset = [ACE.SWEPAM(), ACE.MAG(), ACE.SIS(), ACE.EPAM()]
-        dataset =  list(map(dataset, lambda x: x.data_prep(scrap_date, step_size)))
+        dataset =  list(map(lambda x: x.data_prep(scrap_date, step_size), dataset))
         return pd.concat(dataset, axis = 1) ## TODO REVISAR LOS PREPS
 
 class DSCOVRDataset(L1DatasetLevel1):
@@ -139,7 +142,7 @@ class DSCOVRDataset(L1DatasetLevel1):
 class SOHODataset(L1DatasetLevel1):
     def prepare_data(self, scrap_date, step_size) -> pd.DataFrame:
         dataset = [SOHO.CELIAS_PM(), SOHO.CELIAS_SEM()]
-        dataset =  list(map(dataset, lambda x: x.data_prep(scrap_date, step_size)))
+        dataset =  list(map(lambda x: x.data_prep(scrap_date, step_size), dataset))
         return pd.concat(dataset, axis = 1) ## TODO REVISAR LOS PREPS
 
 class DstDataset(L1DatasetLevel1):
@@ -157,16 +160,15 @@ class SwarmDataset(L1DatasetLevel1):
             l1: bool = True,
     ) -> None:
         super().__init__(dataset, scrap_date, sequence_length, dst_sl, step_size, l1)
-        self.idx = lambda idx: DstBasedIdx.SWARMIdx(idx, scrap_date[0], sequence_length)
+        self.idx = lambda idx: DstBasedIdx.SWARMIdx(idx, scrap_date[0])
         self.scrap_date_list = DstBasedScrap.SWARM(scrap_date, sequence_length)
     def prepare_data(self, scrap_date, step_size) -> pd.DataFrame:
-        # data prep
         dataset = [
             SWARM.MAG_ION(scrap_date, step_size),
             SWARM.EFI(scrap_date, step_size),
             SWARM.FAC(scrap_date, step_size),
         ]
-        dataset =  list(map(dataset, lambda x: x.data_prep(scrap_date, step_size)))
+        dataset =  list(map(lambda x: x.data_prep(scrap_date, step_size), dataset))
         return pd.concat(dataset, axis = 1) ## TODO REVISAR LOS PREPS
 
 class _ImageryBase(Dataset):
@@ -177,14 +179,29 @@ class _ImageryBase(Dataset):
             sequence_length: timedelta,
             dst_sl: timedelta,
     ) -> None:
+        self.root_path: str = root_path
+        self.path_to_image: Callable[[str], str] = lambda name: osp.join(self.root_path, f"{name}.png")
+        self.raw_dataset: List[str] = glob.glob(self.path_to_image('*'))
         self.sequence_length = sequence_length
         self.len_dataset = dataset_len(scrap_date, dst_sl)
         self.idx = lambda idx: DstBasedIdx.SunIdx(idx, scrap_date[0], sequence_length)
         self.scrap_date_list = DstBasedScrap.Sun(scrap_date, sequence_length)
+        interval_time: List[str] = datetime_interval(*self.scrap_date_list, timedelta(days = 1))
+        self.data: Dict[str, List[str]] = {datetime.strptime(key, '%Y%m%d'): [path for path in self.raw_dataset if key in path] for key in interval_time}
+        self.transforms: tt.Compose = tt.Compose([
+            tt.PILToTensor(),
+            tt.Resize(256),
+            tt.Normalize() ## setup the values
+        ])
     def __len__(self):
         return self.len_dataset
-    def __getitem__(self, idx) -> Tensor:
-        return torch.from_numpy(self.dataset[self.idx(idx)].values)
+
+    def __getitem__(self, idx: int | slice) -> Tensor:
+        idx = self.idx(idx)
+        img_paths: List[List[str]] = list(self.data.values())[idx]
+        return torch.cat([
+            self.transforms(Image.open(path)) for list_paths in img_paths for path in list_paths
+        ], dim = 1)
 
 # Dataset for all tasks involved
 class LASCODataset(_ImageryBase):
@@ -194,7 +211,7 @@ class LASCODataset(_ImageryBase):
             sequence_length: timedelta,
             dst_sl: timedelta,
     ):
-        super().__init__(root_path, scrap_date, sequence_length, dst_sl)
+        super().__init__('./data/SOHO/LASCO/', scrap_date, sequence_length, dst_sl)
 
 # Dataset for all tasks involved
 class SDODataset(_ImageryBase):
@@ -204,7 +221,7 @@ class SDODataset(_ImageryBase):
             sequence_length: timedelta,
             dst_sl: timedelta,
     ):
-        super().__init__(root_path, scrap_date, sequence_length, dst_sl)
+        super().__init__('./data/SDO/AIA_HR/', scrap_date, sequence_length, dst_sl)
 
 
 # Dataset for all tasks involved
@@ -213,23 +230,24 @@ Supervised synthetic data creation:
 model: SOHO, WIND ----> DSCOVR, ACE
 """
 class DefaultDataModule(LightningDataModule):
-
     def __init__(self, dataset: DatasetLevel3, batch_size: int, num_workers: int, pin_memory: bool, train_p: float) -> None:
         super().__init__()
         self.dataset = dataset
         self.train_p = train_p
         self.batch_size = batch_size
+        self.num_workers: int = num_workers
+        self.pin_memory: int = pin_memory
+
     def setup(self) -> None:
         # Getting the random_split set
         train_len = round(len(self) * self.train_p)
-        val_len = (len(self) - train_len)//2
-        test_len = len(self) - (train_len + val_len)
+        val_len = (len(self.dataset) - train_len)//2
+        test_len = len(self.dataset) - (train_len + val_len)
         # Random split on datasets
         train_ds, val_ds, test_ds = random_split(self.dataset, [train_len, val_len, test_len])
         self.train_ds = train_ds
         self.val_ds = val_ds
         self.test_ds = test_ds
-        #Done
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
@@ -257,95 +275,3 @@ class DefaultDataModule(LightningDataModule):
             num_workers = 12,
             pin_memory=True
         )
-
-class Reconstruction:
-    """
-    # L1 RECONSTRUCTION
-    model: corrupted(DSCOVR, ACE) -> DSCOVR, ACE
-    """
-    class L1Reconstruction(DataModule):
-        """
-        This is for training the VAE that reconstructs
-        the anomaly data for better results.
-        """
-        def __init__(
-                self,
-                base_scrap_date_list: List[datetime],
-                l1_sl: timedelta,
-                dst_sl: timedelta,
-                step_size: timedelta,
-                batch_size: timedelta,
-                train_p: float
-        ) -> None:
-            super().__init__(batch_size, train_p)
-            self.l1_sl = l1_sl
-            self.dst_sl = dst_sl
-            self.step_size = step_size
-            self.base_scrap_date_list = base_scrap_date_list
-
-            for scrap_date in base_scrap_date_list:
-                if scrap_date[0] < datetime(2016, 7, 31) or scrap_date[-1] > datetime(2021, 10, 31):
-                    raise ValueError('Cannot train on this range given the data access')
-
-        def prepare_data(self, scrap_date, step_size) -> None:
-            ## this training will be valid for the current intervals 2016 -> 2021
-            self.l1_ace = ACEDataset(base_scrap_date_list, l1 = True)
-            self.l1_dscovr = DSCOVRDataset(base_scrap_date_list, l1 = True)
-            self.l2_dscovr = DSCOVRDataset(base_scrap_date_list)
-            self.l2_ace = ACEDataset(base_scrap_date_list)
-            self.dataset = MultiDataset(
-                ACEDataset()
-            )
-            assert (len(self.l1_ace) == len(self.l2_ace) == len(self.l2_dscovr) == len(self.l1_dscovr)), 'Invalid datasets debug']
-
-        def __getitem__(self, index) -> Tuple[Tensor, ...]:
-            return (
-                self.l1_ace[index],
-                self.l1_dscovr[index],
-                self.l2_ace[index],
-                self.l2_dscovr[index],
-            )
-
-        def __len__(self):
-            return sum([len(dataset) for dataset in self.dataset])
-
-    class ImageReconstruction(DataModule):
-        """
-        For both LASCO and SDO, and whatever imagery data
-        you have.
-        """
-
-        corrupted = tt.Compose([
-            tt.ToTensor(),
-            tt.RandomErasing(p = 1)
-        ])
-
-        normal = tt.Compose([
-            tt.ToTensor(),
-        ])
-
-        def __init__(
-                self,
-                base_scrap_date_list: List[datetime],
-                dataset,
-                batch_size: int,
-                train_p: float
-        ) -> None:
-            super().__init__(batch_size, train_p)
-            self.dataset = dataset
-            self.base_scrap_date_list = base_scrap_date_list
-
-        def prepare_data(self) -> None:
-            self.dataset = self.dataset(self.base_scrap_date_list)
-            self.dataset.prepare_data()
-
-        def __len__(self) -> int:
-            return len(self.dataset)
-
-        def __getitem__(self, index) -> Tuple[Tensor, Tensor]:
-            x_hat = self.corrupted(index)
-            x = self.normal(index)
-            return x_hat, x
-
-# TODO: Review this code
-# TODO: Define carefully all of the data_prep members
--- synthetic/download/L1.py
+++ synthetic/download/L1.py
@@ -1,19 +1,19 @@
-from datetime import datetime
+from date_prep import general_dates, merge_scrap_date_lists
 from starstream import DSCOVR, WIND, SOHO, ACE
 from starstream.utils import DataDownloading
-from date_prep import general_dates, merge_scrap_date_lists
-import asyncio
 from typing import Tuple, List
+from datetime import datetime
 
 if __name__ == '__main__':
     scrap_date_list: List[Tuple[datetime, datetime]] = merge_scrap_date_lists(
             general_dates('post_2016'),
             general_dates('pre_2016')
     )
-    asyncio.run(DataDownloading(
+
+    DataDownloading(
         [DSCOVR(), ACE.MAG(), ACE.SWEPAM(),
-         ACE.EPAM(), ACE.SIS()
+         ACE.EPAM(), ACE.SIS(),
          SOHO.CELIAS_SEM(), SOHO.CELIAS_PM(),
          WIND.MAG(), WIND.TDP_PM()],
         scrap_date_list
-    ))
+    )
