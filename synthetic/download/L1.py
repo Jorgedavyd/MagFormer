487c3c4060726fbc47e5b2594548bd7740de8e55
Jorgedavyd <jorged.encyso@gmail.com>
2024-09-09 20:51:23 -0400

    default

--- synthetic/download/L1.py
+++ synthetic/download/L1.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from starstream import DSCOVR, WIND, SOHO
+from starstream import DSCOVR, WIND, SOHO, ACE
 from starstream.utils import DataDownloading
 from date_prep import general_dates, merge_scrap_date_lists
 import asyncio
@@ -11,7 +11,8 @@ if __name__ == '__main__':
             general_dates('pre_2016')
     )
     asyncio.run(DataDownloading(
-        [DSCOVR(), # Defining the label
+        [DSCOVR(), ACE.MAG(), ACE.SWEPAM(),
+         ACE.EPAM(), ACE.SIS()
          SOHO.CELIAS_SEM(), SOHO.CELIAS_PM(),
          WIND.MAG(), WIND.TDP_PM()],
         scrap_date_list
